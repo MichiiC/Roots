70d9c75c9cb22bf1c14abc7d8898ee5910390f7e
Mark Koberlein <mkoberlein@gmail.com>
2019-10-16 15:05:23 -0400

    Added animation from Abandon

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -1,11 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class PlayerInput : MonoBehaviour
 {
 
     public GameObject camera;
+    public GameObject descriptionPanel;
+    public GameObject descriptionPanelText;
+
+    public string selectedObject = "";
 
     // Start is called before the first frame update
     void Start() {
@@ -28,72 +34,71 @@ public class PlayerInput : MonoBehaviour
             if (Physics.Raycast(ray, out hit)) {
                 
                 GameObject go = hit.transform.gameObject;
+                Animator cameraAnimator = camera.GetComponent<Animator>();
 
-                if (go.CompareTag("Fork Lift")) {
-                    
-                    Animator cameraAnimator = camera.GetComponent<Animator>();
-                    cameraAnimator.Play("CameraMoveToForkLift");
-                    print("Fork Lift Touched");
+
+                switch (selectedObject)
+                {
+                    case "Fork Lift":
+                        cameraAnimator.Play("CameraMoveBackFromForkLift");
+                        selectedObject = "";
+                        break;
+                    case "Abandon":
+                        cameraAnimator.Play("CameraMoveBackFromAbandon");
+                        selectedObject = "";
+                        break;
+                    default:
+                        break;
                 }
-                if (go.CompareTag("Grass")){
 
-                    Animator cameraAnimator = camera.GetComponent<Animator>();
+                if (go.CompareTag("Fork Lift")) 
+                {
+                    cameraAnimator.Play("CameraMoveToForkLift");
+                    selectedObject = "Fork Lift";
+                }
+                if (go.CompareTag("Grass"))
+                {
                     cameraAnimator.Play("CameraMoveToGrass");
                     print("Grass Touched");
                 }
                 if (go.CompareTag("Abandon"))
                 {
-
-                    Animator cameraAnimator = camera.GetComponent<Animator>();
                     cameraAnimator.Play("CameraMoveToAbandon");
+                    selectedObject = "Abandon";
                     print("Abandon Touched");
                 }
                 if (go.CompareTag("Smoke"))
                 {
-
-                    Animator cameraAnimator = camera.GetComponent<Animator>();
                     cameraAnimator.Play("CameraMoveToSmoke");
                     print("Smoke Touched");
                 }
                 if (go.CompareTag("Lights"))
                 {
-
-                    Animator cameraAnimator = camera.GetComponent<Animator>();
                     cameraAnimator.Play("CameraMoveToLight");
                     print("Lights Touched");
                 }
                 if (go.CompareTag("Dog"))
                 {
-
-                    Animator cameraAnimator = camera.GetComponent<Animator>();
                     cameraAnimator.Play("CameraMoveToDog");
                     print("Dog Touched");
                 }
                 if (go.CompareTag("Tree"))
                 {
-
-                    Animator cameraAnimator = camera.GetComponent<Animator>();
                     cameraAnimator.Play("CameraMoveToTree");
                     print("Tree Touched");
                 }
                 if (go.CompareTag("Ground"))
                 {
-
-                    Animator cameraAnimator = camera.GetComponent<Animator>();
                     cameraAnimator.Play("CameraMoveToGround");
                     print("Ground Touched");
                 }
                 if (go.CompareTag("Well"))
                 {
-
-                    Animator cameraAnimator = camera.GetComponent<Animator>();
                     cameraAnimator.Play("CameraMoveToWell");
                     print("Well Touched");
                 }
                 if (go.CompareTag("Bird"))
                 {
-
-                    Animator cameraAnimator = camera.GetComponent<Animator>();
                     cameraAnimator.Play("CameraMoveToBird");
                     print("Bird Touched");
                 }
